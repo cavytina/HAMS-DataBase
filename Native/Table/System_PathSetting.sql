ef03dd8adda5d9190d60194d5ccee6347792e5d5
cavy <cavy1010@163.com>
2023-01-13 00:13:35 +0800

    modify dict

---
+++ Native/Table/System_PathSetting.sql
@@ -0,0 +1,25 @@
+/*****************************************************
+System_PathSetting                程序框架路径设置
+******************************************************/
+
+drop table if exists System_PathSetting;
+
+CREATE TABLE System_PathSetting 
+(
+    Code VARCHAR primary key,                --代码
+    Item VARCHAR NOT NULL,                   --英文名称
+    Name VARCHAR NOT NULL,                   --中文名称
+    Content VARCHAR DEFAULT '',              --路径
+    Description VARCHAR DEFAULT '',          --
+    Note VARCHAR DEFAULT '',                 --
+    Rank INT NOT NULL,                       --排序
+    Flag BOOLEAN NOT NULL                    --是否启用
+);
+
+INSERT INTO System_PathSetting (Code,Item,Name,Rank,Flag)
+SELECT '01GPKA6WE841SE31MQWH3Y5WNF','DefaultApplictionCatalogue','默认程序运行目录',1,True
+WHERE NOT EXISTS (SELECT 1 FROM System_PathSetting WHERE Code='01GPKA6WE841SE31MQWH3Y5WNF');
+
+INSERT INTO System_PathSetting (Code,Item,Name,Rank,Flag)
+SELECT '01GPKA6WE85VSFC0S16CF7MCBJ','DefaultNativeDataBaseFilePath','默认本地数据库文件路径',2,True
+WHERE NOT EXISTS (SELECT 1 FROM System_PathSetting WHERE Code='01GPKA6WE85VSFC0S16CF7MCBJ');
--- Native/Table/System_VersionSetting.sql
+++ Native/Table/System_VersionSetting.sql
@@ -1,36 +1,41 @@
 /*****************************************************
-System_VersionSetting                程序版本设置
+System_VersionSetting                程序框架版本设置
 ******************************************************/
 
 drop table if exists System_VersionSetting;
 
 CREATE TABLE System_VersionSetting 
 (
-    Code VARCHAR primary key,      --代码
-    Name VARCHAR,                  --英文名称
-    ReferName VARCHAR,             --中文名称
-    Content VARCHAR,               --设置值
-    Description VARCHAR,           --描述
-    Rank INT,                      --排序
-    Flag BOOLEAN                   --是否启用
+    Code VARCHAR primary key,                 --代码
+    Item VARCHAR NOT NULL,                    --英文名称
+    Name VARCHAR NOT NULL,                    --中文名称
+    Content VARCHAR DEFAULT '',               --设置值
+    Description VARCHAR DEFAULT '',           --描述
+    Note VARCHAR DEFAULT '',                  --备注
+    Rank INT NOT NULL,                        --排序
+    Flag BOOLEAN NOT NULL                     --是否启用
 );
 
-INSERT INTO System_VersionSetting (Code,Name,ReferName,Content,Description,Rank,Flag)
-SELECT '01GP83YVRCNNV5CJB9EJR8F33C','VersionNumber','版本号','23.01','',1,True
-WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Name='VersionNumber');
+INSERT INTO System_VersionSetting (Code,Item,Name,Content,Description,Rank,Flag)
+SELECT '01GPGV56ZKV8DHK6AQDPY1B97T','VersionNumber','版本号','23.01','',1,True
+WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Code='01GPGV56ZKV8DHK6AQDPY1B97T');
 
-INSERT INTO System_VersionSetting (Code,Name,ReferName,Content,Description,Rank,Flag)
-SELECT '01GP83YVRCKRZFXXBJTFEZ634Y','VersionCode','当前版本','Standard','',2,True
-WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Name='VersionCode');
+INSERT INTO System_VersionSetting (Code,Item,Name,Content,Description,Rank,Flag)
+SELECT '01GPGV56ZK7H2W407TSQY8VM03','VersionCode','当前版本','标准版','',2,True
+WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Code='01GPGV56ZK7H2W407TSQY8VM03');
 
-INSERT INTO System_VersionSetting (Code,Name,ReferName,Content,Description,Rank,Flag)
-SELECT '01GP83YVRC9M90BNVJBAAQDAFP','OpenSourceAddress','开源地址','https://github.com/cavytina/HAMS','',3,True
-WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Name='OpenSourceAddress');
+INSERT INTO System_VersionSetting (Code,Item,Name,Content,Description,Rank,Flag)
+SELECT '01GPGV56ZMTTET8PECQT9X2X8N','ValidTime','有效时间','2023-01-12','',3,True
+WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Code='01GPGV56ZMTTET8PECQT9X2X8N');
 
-INSERT INTO System_VersionSetting (Code,Name,ReferName,Content,Description,Rank,Flag)
-SELECT '01GP83YVRCGR79139Y8FSHBRX7','OpenSourceProtocol','开源协议','MIT','',4,True
-WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Name='OpenSourceProtocol');
+INSERT INTO System_VersionSetting (Code,Item,Name,Content,Description,Rank,Flag)
+SELECT '01GPGV56ZMR71N1XAXT0QCNP02','OpenSourceAddress','开源地址','https://github.com/cavytina/HAMS','',4,True
+WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Code='01GPGV56ZMR71N1XAXT0QCNP02');
 
-INSERT INTO System_VersionSetting (Code,Name,ReferName,Content,Description,Rank,Flag)
-SELECT '01GP83YVRCWT212M2RCV9XZEQB','Email','邮箱','cavy1010@163.com','',5,True
-WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Name='Email');
+INSERT INTO System_VersionSetting (Code,Item,Name,Content,Description,Rank,Flag)
+SELECT '01GPGV9FGJBMRJ81MQAE06EPAH','OpenSourceProtocol','开源协议','MIT','',5,True
+WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Code='01GPGV9FGJBMRJ81MQAE06EPAH');
+
+INSERT INTO System_VersionSetting (Code,Item,Name,Content,Description,Rank,Flag)
+SELECT '01GPGV9FGJFVY0RYXVEFA9M32C','Email','邮箱','cavy1010@163.com','',6,True
+WHERE NOT EXISTS (SELECT 1 FROM System_VersionSetting WHERE Code='01GPGV9FGJFVY0RYXVEFA9M32C');
