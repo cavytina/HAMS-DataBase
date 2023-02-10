6a714d3baf7ab5175e14aa6a0b7e9aaa386888b7
cavy <cavy1010@163.com>
2023-02-10 23:55:01 +0800

    同步设置System_FrameModuleSetting数据

--- Native/Table/System_FrameModuleSetting.sql
+++ Native/Table/System_FrameModuleSetting.sql
@@ -17,26 +17,34 @@ CREATE TABLE System_FrameModuleSetting
     EnabledFlag BOOLEAN        NOT NULL              --有效标志
 );
 
+INSERT INTO System_FrameModuleSetting (Code,Item,Name,Rank,DefaultFlag,EnabledFlag)
+SELECT '01GRXZPH1F4QPX5A6AEQPEX7HZ','ApplictionModule','主程序模块',1,True,True
+WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GRXZPH1F4QPX5A6AEQPEX7HZ');
+
+INSERT INTO System_FrameModuleSetting (Code,Item,Name,Rank,DefaultFlag,EnabledFlag)
+SELECT '01GRXZPH1FQWQE3277SFNTC9C9','KernelModule','程序核心模块',2,True,True
+WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GRXZPH1FQWQE3277SFNTC9C9');
+
 INSERT INTO System_FrameModuleSetting (Code,Item,Name,Content,Description,Rank,DefaultFlag,EnabledFlag)
-SELECT '01GQMQVKRZ9700BV5PBWMN2633','ServiceModule','程序服务模块','HAMS.Frame.Service.dll',
+SELECT '01GRXZPH1FF6H7FJ73ACCF4QTQ','ServiceModule','程序服务模块','HAMS.Frame.Service.dll',
         'HAMS.Frame.Service.ServiceModule, HAMS.Frame.Service, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null',
-        	1,True,True
-WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GQMQVKRZ9700BV5PBWMN2633');
+        	3,False,True
+WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GRXZPH1FF6H7FJ73ACCF4QTQ');
 
 INSERT INTO System_FrameModuleSetting (Code,Item,Name,Content,Description,Rank,DefaultFlag,EnabledFlag)
-SELECT '01GQMQVKRZSQHKR5P1V9074PEX','LoginModule','程序登录模块','HAMS.Frame.Control.Login.dll',
+SELECT '01GRXZPH1FCQRE4XBM3EPF6W4S','LoginModule','程序登录模块','HAMS.Frame.Control.Login.dll',
         'HAMS.Frame.Control.Login.LoginModule, HAMS.Frame.Control.Login, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null',
-        	2,True,True
-WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GQMQVKRZSQHKR5P1V9074PEX');
+        	4,False,True
+WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GRXZPH1FCQRE4XBM3EPF6W4S');
 
 INSERT INTO System_FrameModuleSetting (Code,Item,Name,Content,Description,Rank,DefaultFlag,EnabledFlag)
-SELECT '01GQMQVKRZ188ZZADCF4NTSWX5','MainHeaderModule','程序主界面顶栏模块','HAMS.Frame.Control.MainHeader.dll',
+SELECT '01GRXZPH1F4MAGS97RXM33JABE','MainHeaderModule','程序主界面顶栏模块','HAMS.Frame.Control.MainHeader.dll',
         'HAMS.Frame.Control.MainHeader.MainHeaderModule, HAMS.Frame.Control.MainHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null',
-        	3,True,True
-WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GQMQVKRZ188ZZADCF4NTSWX5');
+        	5,False,True
+WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GRXZPH1F4MAGS97RXM33JABE');
 
 INSERT INTO System_FrameModuleSetting (Code,Item,Name,Content,Description,Rank,DefaultFlag,EnabledFlag)
-SELECT '01GQMQVKRZM67GVVD36X7TPNCC','MainLeftDrawerModule','程序主界面侧边栏模块','HAMS.Frame.Control.MainLeftDrawer.dll',
+SELECT '01GRXZQR87RRHQ4PZ7KW74NZR5','MainLeftDrawerModule','程序主界面侧边栏模块','HAMS.Frame.Control.MainLeftDrawer.dll',
         'HAMS.Frame.Control.MainLeftDrawer.MainLeftDrawerModule, HAMS.Frame.Control.MainLeftDrawer, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null',
-        	4,True,True
-WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GQMQVKRZM67GVVD36X7TPNCC');
+        	6,False,True
+WHERE NOT EXISTS (SELECT 1 FROM System_FrameModuleSetting WHERE Code='01GRXZQR87RRHQ4PZ7KW74NZR5');
